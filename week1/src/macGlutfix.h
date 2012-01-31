62630f395b3c3452687944c198d6d87ace44c742
yin liu <yinnie.yinnie@gmail.com>
2012-01-31 11:27:14 -0500

    delete files

--- week1/src/macGlutfix.h
+++
@@ -1,9 +0,0 @@
-
-
-
-	extern void MacGLUTFix(bool isScreenSaver);
-	extern void BringAppToFront(void);
-	
-	extern unsigned char * pixelsBelowWindow(int x, int y, int w, int h);
-	
-
--- week1/src/src-1.moved-aside/macGlutfix.h
+++
@@ -1,9 +0,0 @@
-/*
- *  macGlutfix.h
- *  grabScreen_zach
- *
- *  Created by Yin Liu on 1/28/12.
- *  Copyright 2012 __MyCompanyName__. All rights reserved.
- *
- */
-
